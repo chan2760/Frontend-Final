178869f0f6666d77df6fcdb56e09b6c70dbe1716
Chan Nyein Thu <channyeinthu27605@gmail.com>
2026-03-06 11:15:35 +0700

    Fix GitHub Pages deploy and routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/UserProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
         <App />
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
